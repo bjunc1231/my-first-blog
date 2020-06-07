9857059860494e487a8005caaffacb41b974a990
bjunc1231 <overheat2d@naver.com>
2020-06-07 14:36:18 +0900

    My Django Girls app, first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Jun\AppData\Local\Programs\Python\Python38-32\jun\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
